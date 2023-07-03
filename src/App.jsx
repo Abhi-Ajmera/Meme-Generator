7fe8cd10e15fb7e4df600fccfe557c7fb73f4224
Abhishek Ajmera <abhishekajmera17@gmail.com>
2023-07-03 22:39:03 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import Header from "./components/Header";
+import Meme from "./components/Meme";
+
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Meme />
+    </>
+  );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <>
+      <header className="header">
+        <img className="header-img" src="../src/assets/troll-face.png" alt="" />
+        <h2 className="header-text">Meme Generator</h2>
+      </header>
+    </>
+  );
+}
